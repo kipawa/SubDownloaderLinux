d3beeed9b0f28def981ab69af765a75e3d66cf62
Aditya Shankar Tripathi <kipawa@users.noreply.github.com>
2015-06-05 10:46:09 +0530

    Update kipawa_sub_downloader.sh

--- kipawa_sub_downloader.sh
+++ kipawa_sub_downloader.sh
@@ -1,13 +1,11 @@
+#IFS refers to Internal Field Seperator, which is by default set to space. We must set it to new line!
 tmp=$IFS
 IFS="
 "
 
 path=$NAUTILUS_SCRIPT_SELECTED_FILE_PATHS
-exe="~/Desktop/SubDownloader/dist/subdwnld"
 cd ~/Desktop/SubDownloader/dist
 ./subdwnld $path
 
-#echo "Naya" > path.txt
-#echo $path >> path.txt
-#opensubtitles-download 3.2
 IFS=$tmp
+# ^ We have restored the value of IFS
