61c0c6ed045771c73b9d406b6fe2c8014044b3b3
Pablo Almeida de Andrade <pabloalmeidaff9@gmail.com>
2016-02-05 03:17:05 -0200

    More tests and test files

--- src_linux/tests/general_test.py
+++ src_linux/tests/general_test.py
@@ -1,5 +1,5 @@
 import unittest
-from src_linux.subdwnld import is_filetype_supported, subdb_subtitles_exist, path_without_file_extension
+from src_linux.subdwnld import is_filetype_supported, subdb_subtitles_exist, path_without_file_extension, opensubtitles_subs_exist
 
 class GeneralTests(unittest.TestCase):
 
@@ -16,3 +16,7 @@ class GeneralTests(unittest.TestCase):
         self.assertEquals("afile", path_without_file_extension("afile.txt"))
         self.assertEquals("/a/file/with/full/path", path_without_file_extension("/a/file/with/full/path.mkv"))
         self.assertEquals( "a/file/without/extension", path_without_file_extension("a/file/without/extension"))
+
+    def test_opensubtitles_subs_exist(self):
+        self.assertTrue(opensubtitles_subs_exist("dummy_test_files/aFileForWhichThereIsOpenSubtitlesSubs.mkv"))
+        self.assertFalse(opensubtitles_subs_exist("dummy_test_files/aFileForWhichThereIsNoOpenSubtitlesSubs.mkv"))
