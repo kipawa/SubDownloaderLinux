5a411dedd51f38e7d9b2e456ae78e2fe3093f937
Pablo Almeida de Andrade <pabloalmeidaff9@gmail.com>
2016-02-07 17:37:08 -0200

    Added a test to a case where files are jsut too small for the opensub algorithm to work. Also, removed redundant "main" in subdb.

--- tests/opensub_test.py
+++ tests/opensub_test.py
@@ -7,6 +7,5 @@ class OpenSubTest(unittest.TestCase):
 
     def test_hashfunc(self):
         self.assertEqual(calculateHash("test_media_files/breakdance.avi"), "8e245d9679d31e12")
+        self.assertRaises(ValueError, calculateHash, "test_media_files/averysmallfile.mp4")
 
-if __name__ == '__main__':
-    unittest.main()
